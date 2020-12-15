0d6ede52ebc865a2ea6b335866e3c843fc26b50d
Mohammed <me@mhmdaljefri.dev>
2020-12-15 19:40:06 +0300

    carousels api

---
+++ app/api/carousels.ts
@@ -0,0 +1,16 @@
+import { BlitzApiRequest, BlitzApiResponse } from "blitz"
+import db from "db"
+
+const CarouselsApi = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  const carousels = await db.carousel.findMany({
+    where: {},
+    orderBy: {},
+    select: { image: true },
+  })
+  res.status(200).json({
+    message: "message",
+    carousels,
+  })
+}
+
+export default CarouselsApi
--- app/api/countries.ts
+++ app/api/countries.ts
@@ -5,6 +5,7 @@ const CountriesApi = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
   const countries = await db.country.findMany({
     where: {},
     orderBy: {},
+    select: { name: true, image: true, isTurkey: true },
   })
   res.status(200).json({
     message: "message",
