8bb072822e6c54a2f4c856d83d397546dc1e8dca
Hassan Alattas <ihassanalattas@gmail.com>
2020-12-30 12:20:11 +0300

    tippy.css

--- app/components/Tooltip/index.tsx
+++ app/components/Tooltip/index.tsx
@@ -1,17 +1,4 @@
-import React from "react"
-import { Tooltip } from "react-tippy"
-import { Text } from "theme-ui"
-export default function Tippy({ children }) {
-  return (
-    <Tooltip
-      html={<Text>قريبا سيتوفر</Text>}
-      position="top"
-      trigger="click"
-      animation="shift"
-      arrowSize="big"
-      duration={500}
-    >
-      {children}
-    </Tooltip>
-  )
-}
+import "react-tippy/dist/tippy.css"
+import Tooltip from "./Tooltip"
+
+export default Tooltip
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -20,7 +20,6 @@ import "react-toastify/dist/ReactToastify.css"
 import "suneditor/dist/css/suneditor.min.css" // Import Sun Editor's CSS File
 import "react-datepicker/dist/react-datepicker.css"
 import "rc-drawer/assets/index.css"
-import "react-tippy/dist/tippy.css"
 
 //Binding events.
 Router.events.on("routeChangeStart", () => NProgress.start())
