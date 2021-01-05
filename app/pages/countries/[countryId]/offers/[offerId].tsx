e4e1cec46285739ede76c05da240e0bc725a6669
Mohammed <algfry12@gmail.com>
2021-01-06 00:38:57 +0300

    offer details

--- app/pages/countries/[countryId]/offers/[offerId].tsx
+++ app/pages/countries/[countryId]/offers/[offerId].tsx
@@ -20,7 +20,7 @@ const WhatsNew: BlitzPage<{ offer: Offer & { project?: Project } }> = ({ offer }
         </Wrapper>
         <Wrapper sx={{ paddingY: 5 }}>
           <Heading sx={{ fontSize: [5, 6], fontWeight: 700 }}>{offer.name}</Heading>
-          <Heading sx={{ fontSize: 3, fontWeight: 700 }}>{offer.subTitle}</Heading>
+          <Heading sx={{ fontSize: 3, mt: 3, fontWeight: 700 }}>{offer.subTitle}</Heading>
           <HTMLBox html={offer.details} />
         </Wrapper>
       </Box>
