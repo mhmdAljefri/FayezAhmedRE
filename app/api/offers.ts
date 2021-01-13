2b691b7f1ae0e69d3cfa64e8d10b2f77b7d96146
Mohammed <algfry12@gmail.com>
2021-01-13 21:22:58 +0300

    offers api

---
+++ app/api/offers.ts
@@ -0,0 +1,13 @@
+import getOffers from "app/public/offers/queries/getOffers"
+import { BlitzApiRequest, BlitzApiResponse } from "blitz"
+
+const CarouselsApi = async (req: BlitzApiRequest, res: BlitzApiResponse) => {
+  const offers = await getOffers({})
+
+  res.status(200).json({
+    message: "message",
+    offers,
+  })
+}
+
+export default CarouselsApi
