99600469783a04c6563077fcc67ae0fa26675196
Mohammed <algfry12@gmail.com>
2021-02-12 01:23:11 +0300

    slider

--- app/components/IdealDestinations.tsx
+++ app/components/IdealDestinations.tsx
@@ -24,7 +24,27 @@ export default function IdealDestinations({ explores }) {
           وجهات مثالية للجميع
         </Heading>
 
-        <Swiper slidesPerView={3} autoplay={{ delay: 3000, disableOnInteraction: false }} loop>
+        <Swiper
+          breakpoints={{
+            // when window width is >= 320px
+            320: {
+              slidesPerView: 1,
+            },
+            // when window width is >= 480px
+            480: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            // when window width is >= 640px
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
+          slidesPerView={3}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          loop
+        >
           {explores.map(({ title, countryId, image, id }) => (
             <SwiperSlide key={id} virtualIndex={id}>
               <Link passHref href={`/countries/${countryId}/explore/${id}`}>
