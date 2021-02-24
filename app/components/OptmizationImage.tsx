be37b358bd16e49ed8f5faa8ed1e483ec9320a88
mhmdAljefri <algfry12@gmail.com>
2021-02-24 19:20:02 +0300

    image eager fucking load

--- app/components/OptmizationImage.tsx
+++ app/components/OptmizationImage.tsx
@@ -4,8 +4,13 @@ import { makeS3Url } from "app/utils"
 type Props = ImageProps & {
   localImage?: boolean
 }
-export default function OptmizationImage({ localImage = false, src, ...props }: Props) {
+export default function OptmizationImage({
+  localImage = false,
+  src,
+  loading = "eager",
+  ...props
+}: Props) {
   const s3Url = makeS3Url(src)
 
-  return <BlitzImage {...props} decoding="sync" src={localImage ? src : s3Url} />
+  return <BlitzImage {...props} loading={loading} decoding="sync" src={localImage ? src : s3Url} />
 }
