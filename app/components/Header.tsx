46b1b6b212061f84e43c5b946ed3499aa9b4939e
mhmdAljefri <algfry12@gmail.com>
2021-02-24 20:10:40 +0300

    fix header in small screens

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -65,7 +65,8 @@ const Header = ({ sx }: HeaderProps) => {
                 sx={{
                   fontSize: [1, 3],
                   paddingX: 2,
-                  width: 100,
+                  maxWidth: 100,
+                  minWidth: 70,
                   lineHeight: "23px",
                   paddingY: 0,
                   color: "primary",
