cb0d16bfe4017e594bac5a33d8aca820752484b7
mhmdAljefri <algfry12@gmail.com>
2021-03-01 16:45:42 +0300

    hide dropdown on item clicked

--- app/components/Dropdown.tsx
+++ app/components/Dropdown.tsx
@@ -2,6 +2,7 @@ import React, { ReactNode } from "react"
 import { Box, Flex, SxStyleProp } from "theme-ui"
 import { Icon } from "react-icons-kit"
 import { chevronDown } from "react-icons-kit/fa/chevronDown"
+import { useState } from "react"
 
 type Option = { key: string | number; value: string | number; node?: ReactNode }
 
@@ -19,17 +20,19 @@ type DropdownProps = {
 
 const Dropdown = ({ options, title, outterStyle, defaultValue, onChange }: DropdownProps) => {
   const defaultOption = options.find((option) => option.value === defaultValue)
+  const [open, setOpen] = useState(false)
 
   return (
-    <Box sx={{ position: "relative", zIndex: 1111, ...outterStyle }}>
+    <Box
+      onMouseLeave={() => setOpen(false)}
+      sx={{ position: "relative", zIndex: 1111, ...outterStyle }}
+    >
       <Flex
+        onMouseEnter={() => setOpen(true)}
+        onClick={() => setOpen(true)}
         sx={{
           alignItems: "center",
           color: "primary",
-          ":hover + div": {
-            opacity: 1,
-            visibility: "visible",
-          },
         }}
       >
         <Box sx={{ marginInlineEnd: 5 }}>
@@ -39,34 +42,31 @@ const Dropdown = ({ options, title, outterStyle, defaultValue, onChange }: Dropd
           <Icon icon={chevronDown} />
         </Box>
       </Flex>
-      <Box
-        sx={{
-          paddingY: 2,
-          position: "absolute",
-          backgroundColor: "background",
-          opacity: 0,
-          visibility: "hidden",
-          ":hover": {
-            opacity: 1,
-            visibility: "visible",
-          },
-          borderRadius: 15,
-          boxShadow: "default",
-          zIndex: 1111,
-        }}
-      >
-        {options.map((option, index) => (
-          <Box
-            key={index}
-            sx={{ paddingX: 2, cursor: "pointer" }}
-            onClick={() => {
-              onChange(option)
-            }}
-          >
-            {option.node || option.value}
-          </Box>
-        ))}
-      </Box>
+      {open && (
+        <Box
+          sx={{
+            paddingY: 2,
+            position: "absolute",
+            backgroundColor: "background",
+            borderRadius: 15,
+            boxShadow: "default",
+            zIndex: 1111,
+          }}
+        >
+          {options.map((option, index) => (
+            <Box
+              key={index}
+              sx={{ paddingX: 2, cursor: "pointer" }}
+              onClick={() => {
+                onChange(option)
+                setOpen(false)
+              }}
+            >
+              {option.node || option.value}
+            </Box>
+          ))}
+        </Box>
+      )}
     </Box>
   )
 }
